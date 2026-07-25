ad97775b426c0f8951ba0a239cd8ba871608dbc3
kriti <kritim.1990@gmail.com>
2026-07-25 10:31:16 +0530

    Next.js site with Pages pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export",              // build to static HTML in ./out
+  images: { unoptimized: true }, // <Image> without the server optimizer
 };
 
 export default nextConfig;
